48243795fe2b8c8b904d64d40b0f06a25ee9f105
Udonwajnr <udonwajnr10@gmail.com>
2023-11-05 20:36:07 +0100

    commi

--- src/pages/OurProgram.jsx
+++ src/pages/OurProgram.jsx
@@ -17,7 +17,7 @@ const OurProgram = () => {
                <div className='relative tutor h-[600px] px-10 flex items-center'>
                     <div className=' text-left w-[900px] '>
                         <h3 className='text-white font-bold text-[80px]'>Our Programs</h3>
-                        <p className='text-white text-md'>
+                        <p className='text-[20px]'>
                         Welcome to Proliferate.ai's comprehensive educational program, designed to empower students and learners of all ages to reach their full potential. Our program encompasses a wide range of services and resources, tailored to meet your educational needs and goals.
                         </p>
                     </div>
@@ -26,10 +26,10 @@ const OurProgram = () => {
 
         <section className='px-10'>
             <div>
-                    <h2 className='text-[40px] text-blue-bg'>What sets our program Apart</h2>
-                    <p>At Proliferate.ai, we believe in the transformative power of education. Our program is designed to provide you with the tools, support, and opportunities you need to excel in your educational journey.</p>
+                    <h2 className='text-[40px] text-blue-bg font-bold'>What sets our program Apart</h2>
+                    <p className='text-[20px]'>At Proliferate.ai, we believe in the transformative power of education. Our program is designed to provide you with the tools, support, and opportunities you need to excel in your educational journey.</p>
             </div>
-            <div className='grid grid-cols-3 gap-4'>
+            <div className='grid grid-cols-3 gap-4 my-4'>
                
                 <div className='border rounded-md px-4 py-5  bg-[#FAFAFA]'>
                     <div className='flex flex-col justify-center items-center space-y-4'>
@@ -75,85 +75,85 @@ const OurProgram = () => {
         </section>
 
 
-        <section className='relative px-10 h-[800px]'>
+        <section className='relative px-10 pb-32 mt-14'>
            <div>
-                <div className='w-40 h-40 bg-blue-bg absolute top-0'></div>
+                <div className='w-40 h-40 bg-blue-bg absolute top-0 rounded-2xl'></div>
             
-                <div className='relative py-16 px-32 top-9 bg-white shadow-lg mx-24 my-4'>
-                    <div className='my-5'>
-                        <h2 className='text-blue-bg font-bold text-[35px]'>Key Features of Our Program.</h2>
-                        <p className='font-bold text-[18px]'>Our program encompasses a variety of key features to enhance your learning experience:</p>
+                    <div className='relative py-16 px-32 top-11 bg-white shadow-2xl mx-24 my-4'>
+                        <div className='my-5'>
+                            <h2 className='text-blue-bg font-bold text-[35px]'>Key Features of Our Program.</h2>
+                            <p className='font-bold text-[18px]'>Our program encompasses a variety of key features to enhance your learning experience:</p>
+                        </div>
+                        
+                        <ul className='list-decimal text-[20px] flex flex-col space-y-5'>
+                            <li className='text-blue-bg font-bold'>
+                                One-on-One Tutoring:
+                                <ul className='list-disc text-black font-bold ml-7'>
+                                    <li>
+                                        Personalized support with dedicated tutors.
+                                    </li>
+
+                                    <li>
+                                        Group Tutoring: Collaborative learning in tutor-led group sessions.
+                                    </li>
+                                    <li>
+                                        Subject-Specific Tutoring: Support for a wide range of subjects and topics.
+                                    </li>
+                                </ul>
+                            </li>
+
+                            <li className='text-blue-bg font-bold'>
+                                    Educational Resources
+                                <ul className='list-disc text-black font-bold ml-7'>
+                                    <li>
+                                    Library of Materials: Access to textbooks, study guides, and educational materials.
+                                    </li>
+
+                                    <li>
+                                    Online Resources: A vast collection of online resources to aid your learning.
+                                    </li>
+                                </ul>
+                            </li>
+
+                            <li className='text-blue-bg font-bold'>
+                            Personalized Support
+                                <ul className='list-disc text-black font-bold ml-7'>
+                                    <li>
+                                    Mentoring and Academic Guidance: Mentorship to help you set and achieve educational and career goals.
+                                    </li>
+
+                                    <li>
+                                    Career and College Counselling: Guidance on college applications and career choices.
+                                    </li>
+                                </ul>
+                            </li>
+
+                            <li className='text-blue-bg font-bold'>
+                            Flexible Learning Options
+                                <ul className='list-disc text-black font-bold ml-7'>
+                                    <li>
+                                        Self-Paced Learning: Study at your own pace using our resources.
+                                    </li>   
+                                    <li>
+                                    Test Preparation: Courses and materials for standardized tests.
+                                    </li>
+                                </ul>
+                            </li>
+
+                        </ul>
                     </div>
-                    
-                    <ul className='list-decimal text-[20px] flex flex-col space-y-5'>
-                        <li className='text-blue-bg font-bold'>
-                            One-on-One Tutoring:
-                            <ul className='list-disc text-black font-bold ml-7'>
-                                <li>
-                                    Personalized support with dedicated tutors.
-                                </li>
-
-                                <li>
-                                    Group Tutoring: Collaborative learning in tutor-led group sessions.
-                                </li>
-                                <li>
-                                    Subject-Specific Tutoring: Support for a wide range of subjects and topics.
-                                </li>
-                            </ul>
-                        </li>
-
-                        <li className='text-blue-bg font-bold'>
-                                Educational Resources
-                            <ul className='list-disc text-black font-bold ml-7'>
-                                <li>
-                                Library of Materials: Access to textbooks, study guides, and educational materials.
-                                </li>
-
-                                <li>
-                                Online Resources: A vast collection of online resources to aid your learning.
-                                </li>
-                            </ul>
-                        </li>
-
-                        <li className='text-blue-bg font-bold'>
-                        Personalized Support
-                            <ul className='list-disc text-black font-bold ml-7'>
-                                <li>
-                                Mentoring and Academic Guidance: Mentorship to help you set and achieve educational and career goals.
-                                </li>
-
-                                <li>
-                                Career and College Counselling: Guidance on college applications and career choices.
-                                </li>
-                            </ul>
-                        </li>
-
-                        <li className='text-blue-bg font-bold'>
-                        Flexible Learning Options
-                            <ul className='list-disc text-black font-bold ml-7'>
-                                <li>
-                                    Self-Paced Learning: Study at your own pace using our resources.
-                                </li>   
-                                <li>
-                                Test Preparation: Courses and materials for standardized tests.
-                                </li>
-                            </ul>
-                        </li>
-
-                    </ul>
-                </div>
                 
-                    <div className=' w-40 h-40 bg-blue-bg absolute bottom-0 right-10 -z-10 '></div>
+                <div className=' w-40 h-40 bg-blue-bg absolute bottom-0 right-10 -z-10 rounded-2xl'></div>
            </div>
         </section>
 
-        <section className=''>
+        <section className='p-10 '>
             <div>
-                <h1>Who Can Benefit from Our Program?</h1>
-                <p>Proliferate.ai's program is designed for a broad spectrum of students and learners, including:</p>
+                <h2 className='text-[40px] text-blue-bg font-bold'>Who Can Benefit from Our Program?</h2>
+                <p className='text-[20px]'>Proliferate.ai's program is designed for a broad spectrum of students and learners, including:</p>
             </div>
 
-            <div className='flex'>
+            <div className='flex justify-between mt-5'>
                 <Link className='hover:bg-ash cursor-pointer transition duration-100 hover:transition hover:duration-100' to={"/register"}>
                     <div className='border p-5 rounded-xl w-64'>
                         <div className='flex justify-center flex-col items-center'>
@@ -183,7 +183,6 @@ const OurProgram = () => {
                         </div>
                     </div>
                 </Link>
-                
 
             </div>
 
